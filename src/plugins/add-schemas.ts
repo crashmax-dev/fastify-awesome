a7f016eb6af402f72d79090eed48391dd710ea9f
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-06-02 20:57:23 +0800

    chore: update `zod`

--- src/plugins/add-schemas.ts
+++ src/plugins/add-schemas.ts
@@ -1,17 +1,20 @@
 import fp from 'fastify-plugin'
-import type { JsonSchema } from 'fastify-zod'
+
+type JsonSchema = {
+  $id: string
+}
 
 declare module 'fastify' {
   interface FastifyInstance {
-    addSchemas(schemas: unknown[]): void
+    addSchemas(schemas: JsonSchema[]): void
   }
 }
 
 export default fp(async (fastify) => {
-  const addSchemas = (schemas: JsonSchema<''>[]) => {
+  const addSchemas = (schemas: JsonSchema[]) => {
     for (const schema of schemas) {
       fastify.addSchema(schema)
-      fastify.log.info(`Schema loaded: ${schema.$id}`)
+      fastify.log.info(schema, 'Schema loaded')
     }
   }
 
